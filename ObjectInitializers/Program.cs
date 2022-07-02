c4367bcfcbad8f55b02cd7604b98a32384770b18
Covali Andrei <krab4ek@gmail.com>
2022-07-02 21:36:38 +0300

    1

--- ObjectInitializers/Program.cs
+++ ObjectInitializers/Program.cs
@@ -1,1 +1,14 @@
-﻿
+﻿using ObjectInitializers;
+
+Console.WriteLine("***** Fun With Object Init Syntax\n");
+
+Point firstPoint = new Point();
+firstPoint.X = 10;
+firstPoint.Y = 10;
+firstPoint.DisplayStats();
+
+Point secondPoint = new Point(20, 20);
+secondPoint.DisplayStats();
+
+Point thirdPoint = new Point() { X=30, Y=30};
+thirdPoint.DisplayStats();
